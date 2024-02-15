6ffdbad87da32f6543d346735424c762f4be480c
richard ditshai <richard.ditshai@gmail.com>
2024-02-15 11:22:11 +0200

    Added Next Link inside a button to add auth link

--- components/nav/NavBar.tsx
+++ components/nav/NavBar.tsx
@@ -1,15 +1,16 @@
+import Link from "next/link";
 import { Button } from "../ui/button";
 import Logo from "./Logo";
 import UserLink from "./UserLink";
 
 export default function Navbar() {
-    let session = { user: false};
+    let session = { user: null}
     return(
         <div className="border-b border-b-white">
             <div className="container flex justify-between items-center h-[74px]">
                 <Logo />
                 {
-                    session.user ? <UserLink />: <Button variant={'default'}>Sign In</Button>
+                    session.user ? <UserLink />: <Button variant={'default'}><Link href={'/auth'}>Sign In</Link></Button>
 
                 }
                 
